b553e2b613abdb554b21a79b02166e746eadf26d
Vladyslav Radchenko <vladcommunication@gmail.com>
2021-07-17 11:02:48 +0200

    Vue commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+
+  <body id="app">
+       <HeaderFlow/>
+
+      <section class="flow-container">
+        <div class="flow-row">
+         <Contact/>
+          </div>
+        </section>
+       <FooterFlow/>
+  
+  
+  </body>
+
+</template>
+
+
+<script>
+import Vue from 'vue'
+import HeaderFlow from './components/HeaderFlow.vue'
+/*
+import Hero from './components/Hero.vue'
+import Products from './components/Products.vue'
+
+ import Popup from './components/Popup.vue' */
+/*import Contact from './components/Contact.vue' */
+import Contact from './components/Contact.vue'
+import FooterFlow from './components/FooterFlow.vue'
+/* Bootstrap load */
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+/* Popper load */
+/* import { createPopper } from '@popperjs/core'; */
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+
+
+
+require('@/assets/css/styles.scss') 
+export default {
+  name: 'App',
+  components: {
+     HeaderFlow,
+     Contact,
+     FooterFlow,
+ 
+    }
+   
+  }
+
+
+
+</script>
+
+
